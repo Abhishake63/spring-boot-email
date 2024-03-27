6851a8bfcd578fa53c0e3cef72e27e26e3207e3b
Abhishake Sen <abhishake.gupta@dynamicmalloc.xyz>
2024-03-27 20:10:41 +0600

    formatted email controller

--- src/main/java/com/springboot/email/controller/EmailController.java
+++ src/main/java/com/springboot/email/controller/EmailController.java
@@ -24,5 +24,4 @@ public class EmailController {
     }
 
     // curl -X POST -H 'Content-Type: application/json' -d '{"to": "<RECIPIENT_MAIL>", "subject": "test subject", "body": "test body"}' http://localhost:8081/api/sendMail -w "\n"
-
 }
